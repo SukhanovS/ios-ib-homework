2335e1e13a21367ee1bce2c51b4de23a9cfdc6a5
SukhanovS <ssukhanov7@gmail.com>
2022-08-13 16:01:47 +0300

    Добавил файл ProfileView.swift и аутлеты

---
+++ Netology_IB_Instrument/HW/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instrument
+//
+//  Created by Stas Sukhanov on 13.08.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var birthday: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var text: UITextView!
+}
